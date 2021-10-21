a84b6ab6be37d9b13c880789462ed737adba9188
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-10-21 14:50:56 -0300

    Mensagem view e utilizando herença e protected

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,12 +1,7 @@
 import { Negociacoes } from './../models/negociacoes.js';
+import { View } from './view.js';
 
-export class NegociacoesView {
-
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.getElementById(seletor);
-    }
+export class NegociacoesView extends View {
 
     template(model: Negociacoes): string {
         return `
@@ -37,7 +32,6 @@ export class NegociacoesView {
 
     update(model: Negociacoes): void {
         const template = this.template(model)
-        console.log(template)
         this.elemento.innerHTML = template;
     }
 }
