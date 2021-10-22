4548d973a1caa31ea61f53456ea684e8653e5d9e
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-10-22 08:53:03 -0300

    Aplicando herança e generics para otimização de código

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,4 +2,11 @@ export class View {
     constructor(seletor) {
         this.elemento = document.getElementById(seletor);
     }
+    template(model) {
+        throw Error('Classe filha precisa implementar o método template');
+    }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
 }
