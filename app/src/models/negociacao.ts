aff7133e75237ca6140765f6f3fffd92af5e2daa
André Bez Birolo <andrecentrobrasileiro@outlook.com>
2021-10-26 14:37:52 -0300

    Interface com generics

--- app/src/models/negociacao.ts
+++ app/src/models/negociacao.ts
@@ -1,6 +1,7 @@
+import { Comparavel } from './../interfaces/comparavel.js';
 import { Imprimivel } from "../utils/imprimivel.js";
 
-export class Negociacao implements Imprimivel {
+export class Negociacao implements Imprimivel, Comparavel<Negociacao> {
 
     constructor(
         private _data: Date, 
@@ -38,7 +39,6 @@ export class Negociacao implements Imprimivel {
             valor
         );
     }
-
     
     public ehIgual(negociacao: Negociacao): boolean {
         return this.data.getDate() == negociacao.data.getDate()
